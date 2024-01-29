68e606e9c7006884999ec2b267e00a66a9fd3789
kaeylandri <keypengenturu@gmail.com>
2024-01-29 13:35:40 +0700

    array lemari

---
+++ fungsi.js
@@ -0,0 +1,16 @@
+var x = 25;
+var y = 3.14;
+
+var toNum = parseInt(x);
+var toFloat = parseFloat(y);
+console.log(x + toNum);
+
+function jumlah(a, b) {
+    return a + b;
+}
+var kurang = function (a, b) {
+    return a - b;
+};
+
+alert(jumlah(5, 20));
+alert(jumlah(5,20));
