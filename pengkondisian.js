b7430a1bbefb5a7b358b8b847fdd70f6002acf91
kaeylandri <keypengenturu@gmail.com>
2024-01-29 14:07:46 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("masukan angka :");
+//alert("selamat datang paduuka raja" + nama);
+
+if(x % 2 == 0) {
+    alert(x + " bilangan genap");
+} else if (x % 2 == 1) {
+    alert(x + " bilangan ganjil");
+} else {
+    alert("data bukan angka");
+}
