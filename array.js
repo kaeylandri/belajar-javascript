30bc12c5d7d7d8b3587a22bd3c8c5a85f1d79764
kaeylandri <keypengenturu@gmail.com>
2024-01-30 09:09:13 +0700

    object eksperimen

--- array.js
+++
@@ -1,8 +0,0 @@
-// var namaarray = new array()
-var lemari = new Array("dompet", 50000, true);
-console.log(lemari[1]);
-console.log(lemari[2]);
-
-lemari[3] = "ktp";
-lemari.push("ATM");
-console.log(lemari);
--- objek.js
+++
@@ -1,6 +0,0 @@
-var orang = new Object();
-orang.nama = "asep vesmet";
-orang.alamat = "garut";
-orang.noHp = "0852452562";
-alert(orang.nama);
-console.log(orang);
