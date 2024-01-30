8f8dba73810be68c5c927dfccfed78ee7a515228
kaeylandri <keypengenturu@gmail.com>
2024-01-30 07:04:51 +0700

    8b

--- 8b.js
+++ 8b.js
@@ -1,13 +1,20 @@
-alert("menu makanan \n1.mie ayam RP.5000\n2.nasi goreng yajuj majuj RP.15000\n3.seblak ceker dajjal RP.25000");
+alert("menu makanan \n1.mie ayam RP.5000\n2.nasi goreng yajuj majuj RP.15000\n3.seblak RP.25000");
 var makanan = prompt("selamat datang, silakan memesan");
 var pesanan = prompt("mau pesan berapa");
-var x = 15000
-f ("makanan = Nasi Goreng yajuj majuj"){
-    alert(makanan +" \n Total = Rp. "+ x*pesanan );
-}else if ("makanan = Mie Ayam"){
-    alert(makanan +" \n Total = Rp. "+ y*pesanan );
-}else if ("makanan = seblak ceker dajjal"){
-    alert(makanan +" \n Total = Rp. "+ z*pesanan );
-}
+var x = 5000
+var y = 15000
+var z = 25000
+var totalHarga = x * pesanan;
+if (makanan == "mie ayam") {
+  totalHarga = x * pesanan;
+} else if (makanan == "nasi goreng yajuj majuj") {
+  totalHarga = y * pesanan;
+} else if (makanan == "seblak") {
+    totalHarga = z * pesanan;
+  }
+
+alert("Pesanan Anda:\nMakanan: " +
+ makanan +"\nJumlah porsi: " + pesanan + 
+"\nTotal harga: Rp. " + totalHarga );
 
 alert ("Terimakasih sudah mampir");
